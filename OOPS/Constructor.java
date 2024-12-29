a7eacb24fb995cdf8526e8f1a4d839fc576fdd47
Hiiamghana <dbiswajit0712@gmail.com>
2024-12-30 01:39:07 +0530

    types of constructors

--- OOPS/Constructor.java
+++ OOPS/Constructor.java
@@ -1,29 +1,82 @@
 package OOPS;
 
+//class Person {
+//    String name;
+//    int age;
+//
+//    public void printInfo() {
+//        System.out.println(this.name);
+//        System.out.println(this.age);
+//    }
+//
+//    // Non paramiterise Constructor
+//    Person() {
+//        System.out.println("This is the constructor");
+//    }
+//}
+//
+//public class Constructor {
+//    public static void main(String[] args) {
+//        // Create an instance of the Person class
+//        Person p1 = new Person();
+//        p1.name = "Biswajit";
+//        p1.age = 20;
+//
+//        // Call the printInfo method
+//        p1.printInfo();
+//    }
+//}
 
-class Constructor{
-    String Name;
-    int Age;
+//// peramiterise constructor
+//class Person {
+//    String name;
+//    int age;
+//
+//    public void printInfo() {
+//        System.out.println(this.name);
+//        System.out.println(this.age);
+//    }
+//    Person(String name, int age){
+//        this.name=name;
+//        this.age=age;
+//    }
+//}
+//
+//    public class Constructor {
+//        public static void main(String[] args) {
+//            Person p1=new Person("Biswajit",20);
+//
+//
+//            p1.printInfo();
+//        }
+//    }
+
+// Copy Constructor
+class Person{
+    String name;
+    int age;
 
     public void printInfo(){
-        System.out.println(this.Name);
-        System.out.println(this.Age);
+        System.out.println(this.name);
+        System.out.println(this.age);
     }
-//    Constructor(){// this is non peramiterige constructor
-//        System.out.println("constructor called");// create a constructor
-//    }
-    Constructor(String Name,int Age){
-        this.Name=Name;
-        this.Age=Age;
+    Person(Person p2){// copy method
+        this.name= p2.name;
+        this.age= p2.age;
+    }
+    Person(){
+
     }
 
+}
+public class Constructor{
+    public static void main(String[] args) {
+        Person p1=new Person();
+        p1.name="Biswajit";
+        p1.age=20;
 
-    public static void main(String args[]){
-        Constructor student1=new Constructor("biswajit",20);
-//        student1.Name="Biswajit";
-//        student1.Age=20; this are not use in peramiterige constructor
-        student1.printInfo();
+        Person p2=new Person(p1);
 
-
+        p2.printInfo();
     }
 }
